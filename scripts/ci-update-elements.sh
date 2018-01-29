38b9cc42a1650a113e2d0786015c823e653c0e29
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-01-30 11:15:16 +1300

    Adjusted curl command to use a private token and url encode data.

--- scripts/ci-update-elements.sh
+++ scripts/ci-update-elements.sh
@@ -54,7 +54,7 @@ if [[ $TAG =~ $VERSION_REGEX ]]; then
 
   # Submit the release notes.
   echo "Submitting release notes."
-  curl -X POST https://gitlab.wgtn.cat-it.co.nz/api/v4/projects/1077/repository/tags/$NEW_TAG/release -d "tag_name=$NEW_TAG&description=$(echo -e $RELEASE_NOTES)"
+  curl -X POST https://gitlab.wgtn.cat-it.co.nz/api/v4/projects/1077/repository/tags/$NEW_TAG/release --header "PRIVATE-TOKEN: $PROJECT_TOKEN" --data-urlencode "tag_name=$NEW_TAG" --data-urlencode "description=$(echo -e $RELEASE_NOTES)"
 
 # Tag isn't a version number.
 else
