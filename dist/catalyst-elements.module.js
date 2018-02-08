47194fbd790f3fd6769b9f080b7050864a56dc9d
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-02-08 13:28:56 +1300

    Added toggle switch element.

--- dist/catalyst-elements.module.js
+++ dist/catalyst-elements.module.js
@@ -1,6 +1,7 @@
 // Import the catalyts elements.
 import { CatalystFlipButton } from '../../catalyst-flip-button/dist/catalyst-flip-button.module.js';
 import { CatalystToggleButton } from '../../catalyst-toggle-button/dist/catalyst-toggle-button.module.js';
+import { CatalystToggleSwitch } from '../../catalyst-toggle-switch/dist/catalyst-toggle-switch.module.js';
 
 /**
  * Register all the elements in this bundle.
@@ -8,11 +9,13 @@ import { CatalystToggleButton } from '../../catalyst-toggle-button/dist/catalyst
 function registerCatalystElements() {
   CatalystFlipButton.register();
   CatalystToggleButton.register();
+  CatalystToggleSwitch.register();
 }
 
 // Export all the catalyst elements and the `registerCatalystElements` function.
 export {
   registerCatalystElements,
   CatalystFlipButton,
-  CatalystToggleButton
+  CatalystToggleButton,
+  CatalystToggleSwitch
 };
--- src/catalyst-elements.js
+++ src/catalyst-elements.js
@@ -1,3 +1,4 @@
 /* eslint no-unused-vars: 0 */
 import { CatalystFlipButton } from '../node_modules/@catalyst-elements/catalyst-flip-button/dist/catalyst-flip-button.module.js';
 import { CatalystToggleButton } from '../node_modules/@catalyst-elements/catalyst-toggle-button/dist/catalyst-toggle-button.module.js';
+import { CatalystToggleSwitch } from '../node_modules/@catalyst-elements/catalyst-toggle-switch/dist/catalyst-toggle-switch.module.js';
