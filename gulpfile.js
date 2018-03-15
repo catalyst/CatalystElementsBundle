f7be6b9fa0ecfceee866013b34b9e8c7302f92ff
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2018-03-15 16:48:10 +1300

    Removed shell scripts in favor of node scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,4 @@ require('./tasks/clean.js');
 require('./tasks/build.js');  // Task: build
 require('./tasks/analyze');   // Task: analyze
 require('./tasks/docs.js');   // Task: build-docs
+require('./tasks/fix-dependencies.js'); // Task: fix-dependencies
